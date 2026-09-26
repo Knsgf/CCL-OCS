ebbaa03fda80b68985553ad7f01fea9e123219e2
Knsgf <kf177045@hotmail.com>
2026-09-26 09:55:51 +0300

    Moved assert to separate file

--- catenary_for_CCL.importer/Main.cs
+++ catenary_for_CCL.importer/Main.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
 using HarmonyLib;
@@ -96,23 +94,4 @@ internal class test_component: SimComponent
             }
         }
     }
-}
-
-internal static class assert
-{
-    [Conditional("DEBUG")]
-    public static void test([DoesNotReturnIf(false)] bool passed)
-    {
-        if (!passed)
-            throw new assertion_failed_exception();
-    }
-}
-
-internal class assertion_failed_exception: Exception
-{
-    public assertion_failed_exception(): base()
-    {}
-
-    public assertion_failed_exception(string message): base(message)
-    {}
 }
---
+++ catenary_for_CCL.importer/assert.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace catenary_for_CCL.importer;
+
+internal static class assert
+{
+    [Conditional("DEBUG")]
+    public static void test([DoesNotReturnIf(false)] bool passed)
+    {
+        if (!passed)
+            throw new assertion_failed_exception();
+    }
+}
+
+internal class assertion_failed_exception: Exception
+{
+    public assertion_failed_exception(): base()
+    {}
+
+    public assertion_failed_exception(string message): base(message)
+    {}
+}
